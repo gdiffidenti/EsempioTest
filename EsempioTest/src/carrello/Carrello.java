d34a567fd30aac91d193a413fdb7be550021729d
gdiffidenti <giovannidiffidenti@localhost>
2021-12-20 15:11:40 +0100

    Primo commit

---
+++ EsempioTest/src/carrello/Carrello.java
@@ -0,0 +1,28 @@
+package carrello;
+
+public class Carrello {
+private int amount;
+	
+	
+	
+	public Carrello() {
+		amount = 0;
+	}
+	
+	public int getAmount() {
+		return amount;
+	}
+	
+	public void insertOneProduct() {
+		amount++;
+	}
+	
+	public void removeOneProduct() {
+		if(amount > 0)
+		amount--;
+	}
+
+}
+
+
+
---
+++ EsempioTest/src/carrello/CarrelloTest.java
@@ -0,0 +1,68 @@
+package carrello;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CarrelloTest {
+	
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+
+	@Test
+	public void testCarrello() {
+		
+		Carrello carrello = new Carrello();
+		int expected = 0;
+		assertEquals(expected, carrello.getAmount());
+		
+	}
+
+	@Test
+	public void testGetAmount() {
+	
+	}
+
+	@Test
+	public void testInsertOneProduct() {
+		Carrello carrello = new Carrello();
+		int expected = 3;
+		carrello.insertOneProduct();
+		carrello.insertOneProduct();
+		carrello.insertOneProduct();
+		assertEquals(expected, carrello.getAmount());
+	}
+
+	@Test
+	public void testRemoveOneProduct() {
+		Carrello carrello = new Carrello();
+		int expected = 0;
+		carrello.removeOneProduct();
+		assertEquals(expected, carrello.getAmount());
+		
+		
+	}
+
+}
